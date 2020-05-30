07ba2905738e55af761d367a77a6fbccb5145759
Kumar Jitendra <log2jeet24@gmail.com>
2020-05-31 04:40:57 +1000

    updated commit 31 May

--- src/test/java/pages/Dashboard.java
+++ src/test/java/pages/Dashboard.java
@@ -5,6 +5,7 @@ import net.thucydides.core.annotations.Step;
 public class Dashboard {
 
 	OrangeHRMDashboard dashboard;
+	Newtourlogindashboard dashboardnewtour ;
 	
 	@Step
 	public void verifyAdmin()
@@ -12,4 +13,29 @@ public class Dashboard {
 		dashboard.loginVerified();
 	}
 	
+	@Step
+	public void selectflightround () throws Exception {
+		
+		dashboardnewtour.SelectFlightype();
+	}
+	
+	@Step
+	public void selectPassenger() throws Exception {
+		
+		dashboardnewtour.SelectPassengertype();
+	}
+	
+	
+	
+	@Step
+	public void clickcontinue() throws Exception {
+		
+		dashboardnewtour.ClickonContinue();
+	}
+	
+	@Step
+	public void signoff () throws Exception {
+		
+		dashboardnewtour.SignOff();
+	}
 }
--- src/test/resources/features/search/loginToApplication.feature
+++ src/test/resources/features/search/loginToApplication.feature
@@ -5,3 +5,14 @@ Feature: Login To HRM
 	When user enter admin as username
 	And  user enter admin123 as password
 	Then user should be able to login 
+	
+	
+@Newtoursdemo
+Scenario: To login and verify the user booking of the newtours demo 
+
+Given User is on Home page 
+When  User Enters 'log2jeet24@gmail.com' and 'Ajiten#1990'
+And   User Selects Flight type 
+And   User Select Passenger
+And   User Clicks on the Continue Button 
+Then  User Sign Off from the Home Page 
